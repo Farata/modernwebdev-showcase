9aa9bb616ea47be8bac4adb25c0eda4e5e0251b9
Anton Moiseev <a@antonmoiseev.com>
2014-08-14 02:21:43 +0400

    unit 3: add proposed solution

--- homework3/bower_components/DefinitelyTyped/CONTRIBUTING.md
+++
@@ -1,1 +0,0 @@
-Please see the [contribution guide](https://github.com/borisyankov/DefinitelyTyped/wiki/How-to-contribute) for information on how to contribute to this project.
--- homework3/bower_components/DefinitelyTyped/domready/domready.d.ts
+++
@@ -1,6 +0,0 @@
-// Type definitions for domready
-// Project: https://github.com/ded/domready
-// Definitions by: Christian Holm Nielsen
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
-
-declare function domready(callback: () => any) : void;
